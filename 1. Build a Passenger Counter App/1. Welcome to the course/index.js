07320ea58a49f04edc33fb4570fc21a6df6c08fa
Bilgin Aksoy <bilginaksoy2003@yahoo.com>
2022-05-26 10:58:45 +0300

    End of first lesson

--- 1. Build a Passenger Counter App/1. Welcome to the course/index.js
+++ 1. Build a Passenger Counter App/1. Welcome to the course/index.js
@@ -1,1 +1,19 @@
 // javascript
+//document.getElementById("count-el").innerText = 5
+
+let count = 0 ;
+let countEl = document.getElementById("count-el");
+
+function increment(){
+    count += 1;
+    console.log("Btn clicked");
+    countEl.innerText = count;
+}
+
+function save(){
+    console.log("Count new value is " + count);
+}
+
+let greetEl = document.getElementById("greet-el");
+greetEl.innerText += " JS";
+
